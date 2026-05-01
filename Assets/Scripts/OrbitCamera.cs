a909a79b3bae309f500320b97fb48598121eadd9
Aomigo <bingoash36@gmail.com>
2026-05-01 22:25:19 +0200

    added a recentering camera

--- Assets/Scripts/OrbitCamera.cs
+++ Assets/Scripts/OrbitCamera.cs
@@ -12,18 +12,30 @@ public class OrbitCamera : MonoBehaviour
     [SerializeField, Min(0f)]
     float focusRadius = 1f;
 
+    [SerializeField, Range(0f, 1f)]
+    float focusCentering = 0.5f;
+
+
     Vector3 focusPoint;
+    //Define the base orbital rotation : around x and around y
+    Vector2 orbitAngles = new Vector2(45f, 0f);
 
     void Awake()
     {
-        //Define the position of the object to focus
+        //Define the position of the object to focus, which become the current camera position
         focusPoint = focus.position;
+        //base initial rotation
+        transform.localRotation = Quaternion.Euler(orbitAngles);
     }
-    
+
+
     void LateUpdate()
     {
+        //Updates the camera position
         UpdateFocusPoint();
+        //Get the direction the camera is looking at
         Vector3 lookDirection = transform.forward;
+        //change the position of the camera
         transform.localPosition = focusPoint - lookDirection * distance;
     }
 
@@ -32,17 +44,27 @@ public class OrbitCamera : MonoBehaviour
         //Checks the current position of the object to focus
         Vector3 targetPoint = focus.position;
         //If the radius is superior to 0
-        if(focusRadius > 0f)
+        if (focusRadius > 0f)
         {
-            //Checks the distance between the camera position and the object to focus
+            //Checks the distance between the object to focus and the camera position 
             float distance = Vector3.Distance(targetPoint, focusPoint);
-            //If the distance between the 2 position is higher than the focusRadius
-            if(distance > focusRadius)
+            float t = 1f;
+            //If the camera isnt exactly on the focused object
+            if (distance > 0.001f && focusCentering > 0f)
             {
-                //Makes the camera travel from the current position to the object to focus, with a fRadius/d ratio;
-                focusPoint = Vector3.Lerp(targetPoint, focusPoint, focusRadius / distance);
+                //Keeps the camera 1f - focusCentering % of the way every realLife second
+                t = Mathf.Pow(1f - focusCentering, Time.unscaledDeltaTime);
             }
-        } else
+            //If the distance between the 2 position is higher than the focusRadius
+            if (distance > focusRadius)
+            {
+                //Gets the minimum value between t and fRadius/dist;
+                t = Mathf.Min(t, focusRadius / distance);
+            }
+            //Makes the camera travel from the current position to the object to focus, with the minimum value travel time;
+            focusPoint = Vector3.Lerp(targetPoint, focusPoint, t);
+        }
+        else
         {
             //Else sticks to following perfectly the player
             focusPoint = targetPoint;
