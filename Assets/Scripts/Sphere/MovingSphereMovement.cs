5346a93a31e21e849f037b707be238696be0110e
Aomigo <bingoash36@gmail.com>
2026-05-02 18:46:01 +0200

    slope physic testing, jump towards normal is tbd

--- Assets/Scripts/Sphere/MovingSphereMovement.cs
+++ Assets/Scripts/Sphere/MovingSphereMovement.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class MovingSphere : MonoBehaviour
+public class MovingSphereMovement : MonoBehaviour
 {
 
     [SerializeField, Range(0f, 100f)]
